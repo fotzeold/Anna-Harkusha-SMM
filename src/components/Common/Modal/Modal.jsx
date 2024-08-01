14d4e5d485dc6f33af49f767710f22579ab1272e
Roman Popovych <mechenuyzzzz@gmail.com>
2024-08-01 09:43:16 +0300

    fix scroll

--- src/components/Common/Modal/Modal.jsx
+++ src/components/Common/Modal/Modal.jsx
@@ -2,10 +2,19 @@ import "./Modal.scss"
 import Form from "../Form/Form"
 import { useSelector, useDispatch } from 'react-redux';
 import { closeModal } from "./ModalSlice";
+import { useEffect } from "react";
 
 const Modal = () => {
 	const isOpen = useSelector((state) => state.modal.isOpen);
 	const dispatch = useDispatch();
+
+	useEffect(() => {
+		if (isOpen) {
+			document.body.style.overflow = "hidden"
+		} else {
+			document.body.style.overflow = "auto"
+		}
+	}, [isOpen])
 
 	return (
 		<div className={`modal ${isOpen ? "active" : ""}`}>
--- src/components/Common/Navigation/Navigation.jsx
+++ src/components/Common/Navigation/Navigation.jsx
@@ -1,18 +1,32 @@
 import "./Navigation.scss"
 import Button from "../Button/Button"
+import { useEffect } from "react"
 
 const Navigation = ({ navControll }) => {
 	const { activeNav, toggleActiveNav } = navControll
+
+	useEffect(() => {
+		if (activeNav) {
+			document.body.style.overflow = "hidden"
+		} else {
+			document.body.style.overflow = "auto"
+		}
+	}, [activeNav])
 
 	return (
 		<nav className={`navigation row ${activeNav ? "active" : ""}`}>
 			<a href="#about" onClick={toggleActiveNav}>Про мене</a>
 			<a href="#prices" onClick={toggleActiveNav}>Послуги</a>
 			<a href="#contacts" onClick={toggleActiveNav}>Контакти</a>
-			<Button
-				btnTxt={"Консультація"}
-				btnStyle={{ height: "35px" }}
-			/>
+			<div
+				className="navigation__btn"
+				onClick={toggleActiveNav}
+			>
+				<Button
+					btnTxt={"Консультація"}
+					btnStyle={{ height: "35px" }}
+				/>
+			</div>
 		</nav>
 	)
 }
